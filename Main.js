ad57667a2afb7c85992982caf38f17398b980782
BetoCarrillo <alberto.carrillo01@gmail.com>
2022-07-23 09:09:39 +0200

    JS Exercises 1-8

---
+++ Main.js
@@ -0,0 +1,60 @@
+console.log("Starting javascript...")
+
+//EXERCISE 1
+
+let myDog = "Nano";
+//console.log(myDog);
+
+//EXERCISE 2
+let nanoAge = 17;
+//console.log(nanoAge);
+
+//EXERCISE 3 
+
+let daniAge = 30;
+let ageDiff = (nanoAge - daniAge);
+//console.log(ageDiff);
+
+// EXERCISE 4
+if(nanoAge>=21) {
+    //console.log("Nano is older than 21");
+}
+//else { console.log("Nano is younger than 21") }
+
+//EXERCISE 5
+
+//if (nanoAge < daniAge) { console.log("Nano is younger"); }
+
+//else if (nanoAge > daniAge) { console.log("Nano is older"); }
+
+//else {console.log("Same age"); }
+
+//EXERCISE 6
+
+let classNames = ["Alberto", "Emely", "Jan", "Phillip", "Raul"];
+//console.log(classNames[0])
+//console.log(classNames[4])
+//for (let i = 0; i<classNames.length; i++) { console.log(classNames[i]) };
+
+//EXERCISE 7
+
+const ageClass = [31, 28, 30, 35];
+//let i = 0;
+//while (i < ageClass.length) { console.log(ageClass[i]); i++; }
+
+//while (i < ageClass.length) {
+    //if (ageClass[i] % 2 == 0) console.log(ageClass[i]); i++;}
+
+//for (let i = 0; i < ageClass.length; i++) { console.log(ageClass[i])};
+
+//for (let i = 0; i < ageClass.length; i++)
+    //if (ageClass[i] % 2 == 0) {console.log(ageClass[i])};
+
+
+//EXERCISE 8 
+ 
+function exeight([i]) {
+    console.log ("exeight")
+    return lowest;
+}
+exeight [20,19,200,1,15,]
